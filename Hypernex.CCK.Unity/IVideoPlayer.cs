72b8c7afa6d605c55434785fc4fca489ca91b620
200Tigersbloxed <45884377+200Tigersbloxed@users.noreply.github.com>
2024-05-17 16:25:22 -0400

    Video Player Interfacing

---
+++ Hypernex.CCK.Unity/IVideoPlayer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Hypernex.CCK.Unity
+{
+    public interface IVideoPlayer : IDisposable
+    {
+        /// <summary>
+        /// Defines if the Video Player can be used on the current Platform at all
+        /// </summary>
+        /// <returns>Usable on platform</returns>
+        bool CanBeUsed();
+        /// <summary>
+        /// Defines if the Video Player can be used for the specified source
+        /// </summary>
+        /// <param name="source">The source URL/Path</param>
+        /// <returns>Media usable on Video Player</returns>
+        bool CanBeUsed(Uri source);
+        
+        /// <summary>
+        /// If the Media is playing
+        /// </summary>
+        bool IsPlaying { get; }
+        /// <summary>
+        /// Controls if the audio is muted
+        /// </summary>
+        bool Muted { get; set; }
+        /// <summary>
+        /// Controls of the video is looping
+        /// </summary>
+        bool Looping { get; set; }
+        /// <summary>
+        /// Controls the pitch of the audio
+        /// </summary>
+        float Pitch { get; set; }
+        /// <summary>
+        /// Controls the volume of the audio
+        /// </summary>
+        float Volume { get; set; }
+        /// <summary>
+        /// Controls the position of the media
+        /// </summary>
+        double Position { get; set; }
+        /// <summary>
+        /// Gets the length of the media
+        /// </summary>
+        double Length { get; }
+        /// <summary>
+        /// Sets the source media of the Video
+        /// </summary>
+        string Source { get; set; }
+
+        /// <summary>
+        /// Plays the video
+        /// </summary>
+        void Play();
+        /// <summary>
+        /// Pauses the video
+        /// </summary>
+        void Pause();
+        /// <summary>
+        /// Stops the video
+        /// </summary>
+        void Stop();
+    }
+}
---
+++ Hypernex.CCK.Unity/VideoPlayerDescriptor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hypernex.CCK.Unity
+{
+    /// <summary>
+    /// Describes how to set up the VideoPlayer at setup
+    /// </summary>
+    public class VideoPlayerDescriptor : MonoBehaviour
+    {
+        /// <summary>
+        /// Whether or not to set the emissive map to the texture of the created Render Texture
+        /// </summary>
+        public bool IsEmissive = true;
+
+        /// <summary>
+        /// Output Renderers for the VideoPlayer
+        /// </summary>
+        public List<Renderer> VideoOutputs = new List<Renderer>();
+
+        /// <summary>
+        /// Output Audio for Audio linked to the VideoPlayer
+        /// </summary>
+        public AudioSource AudioOutput;
+
+        /// <summary>
+        /// The shader property for the emission map
+        /// </summary>
+        public string ShaderEmissionProperty = "_EmissionMap";
+        
+        [HideInInspector] public IVideoPlayer CurrentVideoPlayer;
+    }
+}
