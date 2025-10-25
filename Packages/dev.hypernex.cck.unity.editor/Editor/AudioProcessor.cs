2408eff776da1ffdca0fd6b43ebbd56baf06112c
200Tigersbloxed <45884377+200Tigersbloxed@users.noreply.github.com>
2025-10-24 20:06:18 -0400

    package version and audio importer

---
+++ Packages/dev.hypernex.cck.unity.editor/Editor/AudioProcessor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Hypernex.CCK.Unity.Editor
+{
+    public class AudioProcessor : AssetPostprocessor
+    {
+        private void OnPostprocessAudio(AudioClip _)
+        {
+            AudioImporter audioImporter = (AudioImporter) assetImporter;
+            audioImporter.defaultSampleSettings = new AudioImporterSampleSettings
+            {
+                preloadAudioData = true
+            };
+        }
+    }
+}
